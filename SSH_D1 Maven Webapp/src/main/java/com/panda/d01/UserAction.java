3be6c350337b296d92e70477143be10cb8c4b2e1
jj871231 <jj871231@126.com>
2015-08-15 23:22:41 +0800

    D01 --2015-08-15

---
+++ SSH_D1 Maven Webapp/src/main/java/com/panda/d01/UserAction.java
@@ -0,0 +1,19 @@
+package com.panda.d01;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class UserAction extends ActionSupport {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 3951019531929298360L;
+	
+	public String hello()
+	{
+		
+		return "hello";
+	}
+	
+
+}
